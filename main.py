cd9de173ac4fd327e7c85123d9b9921f3d8d2ee7
yuinakorn <yuinakorn@hotmail.com>
2024-01-19 00:59:57 +0700

    upload first

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from controller.main_controller import toggle_process
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/sent_to_cmpho/{option}")
+async def sent_to_cmpho(option: str):
+    return toggle_process(option)
+
