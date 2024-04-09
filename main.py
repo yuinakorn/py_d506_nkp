619bb730bcacda8caacc2fe275bfcd3bdd21c09c
yuinakorn <yuinakorn@hotmail.com>
2024-04-09 20:43:57 +0700

    comment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-
+# ##### เอาไปรวมไว้ใน sendd506 แล้ว อย่าหลงเข้ามาในนี้อีก
 @app.post("/sent_to_cmpho/{option}")
 async def sent_to_cmpho(option: str):
     return toggle_process(option)
